eabea706e4542a0f74ec6046e0940bcb2bcf01b9
wjdtnd <gkssk15963@gmail.com>
2025-03-26 11:47:38 +0900

    250326 @RequestMapping을 이용한 매핑경로

---
+++ src/main/java/kr/ac/kopo/lhn/firstspringboot/controller/Example03Controller.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.lhn.firstspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/test")
+public class Example03Controller {
+
+    @GetMapping(value = "/exam04")
+    public String requestMethod() {
+        return "requestTest";
+    }
+}
